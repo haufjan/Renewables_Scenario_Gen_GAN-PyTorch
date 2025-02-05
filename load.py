7305ea84722cf5e9f4b1bdef04d5d68832c0db9b
haufjan <jan_hauf@web.de>
2025-02-05 17:53:28 +0100

    Update load.py

--- load.py
+++ load.py
@@ -3,32 +3,37 @@ import numpy as np
 
 
 
-#Define function for loading of solar data
+#Define function for loading solar data
 def load_solar_data(path_data: str, path_labels:str) -> tuple:
-    """Load and preprocess data and labels for training the GAN model."""
+    """
+    Load and preprocess solar data and labels for GAN training
+    """
     with open(f'{path_data}' if path_data.endswith('csv') else f'{path_data}.csv', 'r') as csvfile:
         rows = np.array([row for row in csv.reader(csvfile)], dtype=float)
     #Specify according to time points in your own dataset
     rows = rows[:104832,:]
-    print('\nShape rows', rows.shape)
+    print('\nShape rows (data raw):', rows.shape)
 
     with open(f'{path_labels}' if path_labels.endswith('csv') else f'{path_labels}.csv', 'r') as csvfile:
         labels = np.array([row for row in csv.reader(csvfile)], dtype=int)
-    print('Shape labels', labels.shape)
+    print('\nShape labels (labels raw):', labels.shape)
 
-    #Data corresponds to GAN input dimensions (24x24 = 576)
+    #Transform data to conform to GAN image input dimensions (24x24 = 576)
     trX = np.reshape(rows.T,(-1,576))
-    print('\nShape trX',trX.shape)
-    m = np.ndarray.max(rows)
-    print("\nMax(Solar)", m)
+    print('\nShape trX (data preprocessed):',trX.shape)
     trY = np.tile(labels,(32,1))
-    print('\nShape trY', trY.shape)
+    print('\nShape trY (labels preprocessed):', trY.shape)
+    m = np.ndarray.max(rows)
+    print("\nMax(Solar) =", m)
     trX = trX/m
 
     return trX, trY, m
 
 #Define function for loading wind data
 def load_wind_data(path_data: str, path_labels: str) -> tuple:
+    """
+    Load and preprocess solar data and labels for GAN training
+    """
     #Example dataset created for evnet_based GANs wind scenarios generation
     # Data from NREL wind integrated datasets
     with open(f'{path_data}' if path_data.endswith('csv') else f'{path_data}.csv', 'r') as csvfile:
